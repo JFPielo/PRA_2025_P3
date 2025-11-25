6bf69f15fa10872cb997a2aa133e60f79592aff6
JFPIELO1 <jfpielo1@alumno.upv.es@ldsic-vdi12.upvnet.upv.es>
2025-11-25 20:16:24 +0100

    Añadida implementación de la clase TableEntry

---
+++ TableEntry.h
@@ -0,0 +1,50 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+using namespace std;
+
+template <typename V> 
+class TableEntry {
+    public:
+        //Atributos
+   	string key;
+	V value;
+	
+	//Constructor 2 párametros
+	TableEntry(string key, V value){
+		this -> key = key;
+		this -> value = value;
+	}
+	//Constructor 1 parámetro
+	TableEntry(string key){
+		this -> key = key; 
+		this -> valor = NULL;
+	}
+	//Constructor vacío 
+	TableEntry(){
+		this -> key = "";
+		this -> valor = NULL;
+	}
+
+	//Operador ==
+	friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+	 	return (te1.key==te2.key) ? true : false;	
+	}
+	
+	//Operador !=
+	friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+	
+		return (te1.key!=te2.key) ? true : false;	
+	}
+
+	//Operador << 
+	friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+		out << "('" << te.key << "' " << "=>" << te.value << ")";
+		return out;
+	}
+	
+};
+
+#endif
